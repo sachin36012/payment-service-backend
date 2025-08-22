4ee544867fc597e5f0de1334701cc9782b83b532
sachin36012 <sachin36012@gmail.com>
2025-08-22 19:38:58 +0530

    first commit

--- src/main/java/com/payment/config/WebConfig.java
+++ src/main/java/com/payment/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://158.69.211.89")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
